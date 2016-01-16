95084b50904d05adf331eded170d17f6cde0d5be
Noah Yarian <noah.yarian@gmail.com>
2016-01-16 01:06:14 -0600

    ran generator-angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thelmaKiddWebsiteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the thelmaKiddWebsiteApp
+ */
+angular.module('thelmaKiddWebsiteApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thelmaKiddWebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the thelmaKiddWebsiteApp
+ */
+angular.module('thelmaKiddWebsiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
